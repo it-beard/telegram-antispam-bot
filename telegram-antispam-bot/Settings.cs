3fbc847490853a0dd6c2a05e8738d13d75693685
itbeard <iamitbeard@gmail.com>
2021-12-06 00:58:09 +0300

    add iamitbeard to whitelist

--- telegram-antispam-bot/Settings.cs
+++ telegram-antispam-bot/Settings.cs
@@ -3,12 +3,13 @@ namespace TelegramAntispam.Bot;
 public class Settings
 {
     //Add your telegram nickname here
-    public static List<string> WhiteList = new()
+    public static readonly List<string> WhiteList = new()
     {
+        "iamitbeard",
         "SergShadow"
     };
 
-    public static string InfoMessage =
+    public static readonly string InfoMessage =
         "Если ты хочешь отправлять ссылки в комментариях канала АйТиБорода, " +
         "то заходи в репозиторий github.com/it-beard/telegram-antispam-bot и вноси себя в WhiteList (подробнее читай в файле README.md репозитория)";
 }
